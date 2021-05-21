75aecc63a9d90924dcd53aaedae8566f7614283e
huzeyef <72346301+huzeyef@users.noreply.github.com>
2021-05-21 22:07:42 +0300

    main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 //Replace With Your Own Github Username
-const Github_Username = "OlikaX404";
+const Github_Username = "huzeyef";
 
 //Replace With Your Own Email ID
-const Email_Id = "olikaamsalu8@gmail.com";
+const Email_Id = "huzeyef15@gmail.com";
 
 var xhr = new XMLHttpRequest();
 xhr.addEventListener("readystatechange", function() {
